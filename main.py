ee939200b69a483b13ccfd3b892f8ca56183ac08
Saeko <saeko514@outlook.jp>
2025-06-22 09:19:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from datetime import datetime
 import requests
 import os
 
-SHEETY_ID = os.environ["SHEETY_ID"]
+# 環境変数からSheey IDを取得
+SHEETY_ID = os.environ.get("SHEETY_ID")
 SHEETY_ENDPOINT = f"https://api.sheety.co/{SHEETY_ID}/新しい自己肯定感スコアアプリ測定結果/userAgreement"
 
 app = Flask(__name__)
@@ -18,9 +19,11 @@ def index():
 def agreement():
     data = request.json
     print("[POST /api/agreement] 受信データ:", data)
-    user_id = data.get("userId")
-    display_name = data.get("displayName")  
-    timestamp = data.get("agreedAt", datetime.utcnow().isoformat())
+    
+    useragreement = data.get("useragreement", {})
+    user_id = useragreement.get("userId")
+    display_name = useragreement.get("displayName")
+    timestamp = useragreement.get("agreedAt", datetime.utcnow().isoformat())
 
     if not user_id or not display_name:
         return jsonify({"status": "error", "message": "userId または displayName がありません"}), 400
@@ -43,4 +46,4 @@ def agreement():
             return jsonify({"status": "error", "message": response.text}), 500
     except Exception as e:
         print(f"[EXCEPTION] {e}")
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return jsonify({"status":
